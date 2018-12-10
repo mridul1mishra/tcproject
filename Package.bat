b5ef18a5a9907df31e41f6f075aa298535511b53
mridul1m <mridul.mishra@akqa.com>
2018-12-10 10:33:45 +0530

    change3

--- Package.bat
+++ Package.bat
@@ -34,7 +34,7 @@ MKDIR .\_Deployment\Output
 MKDIR .\_Deployment\Output\CAS
 MKDIR .\_Deployment\Output\CDS
 
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" /m /t:Rebuild "test_app.sln" /p:Configuration=%1;ContentAuthoringOutputDir=%~dp0 .\_Deployment\Output\CAS;ContentDeliveryOutputDir=%~dp0 .\_Deployment\Output\CDS
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" /m /t:Rebuild "test_app.sln" /p:Configuration=%1;ContentAuthoringOutputDir=.\%~dp0\_Deployment\Output\CAS;ContentDeliveryOutputDir=.\%~dp0\_Deployment\Output\CDS
 IF ERRORLEVEL 1 GOTO errorHandling
 
 .\packages\nuget\nuget.exe pack ".\test_app\test_app.nuspec" -Version %2 -Verbosity detailed -OutputDirectory .\_Deployment\Packages
